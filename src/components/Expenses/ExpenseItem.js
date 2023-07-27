4dfb540b02d39c843723855af8a9b4054367cf12
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2023-07-27 13:19:10 +0530

    Working With React States

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,20 +6,21 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   // function clickHandler() {}
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
+  const [title, setTitle] = useState(props);
   
   const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
+    setTitle({
+      title: 'Updated!',
+      amount: props.amount + 50,
+    });
   };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <h2>{title.title}</h2>
+        <div className='expense-item__price'>${title.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
